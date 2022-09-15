7962ecc3960cf52afbf1ae4a672308bd13d2acba
sappho <sappho@sappho.io>
2022-09-15 14:52:46 -0400

    add build script

---
+++ build/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+git clean -x -f
+rm -rf sourcemod/
+rm -rf hl2sdk/
+rm -rf mmsource-1.11/
+rm -rf ambuild/
+git clone https://github.com/alliedmodders/sourcemod -b 1.11-dev --recursive
+git clone https://github.com/tf2classic/hl2sdk hl2sdk-tf2 -b tf2
+git clone https://github.com/alliedmodders/metamod-source mmsource-1.11 -b 1.11-dev
+git clone https://github.com/alliedmodders/ambuild
+pip install ./ambuild
+
+cd sourcemod
+mkdir build
+cd build
+
+python3 ../configure.py --enable-optimize -s=tf2 --no-mysql
+ ~/.local/bin/ambuild
+
+
+# we are in the build dir
+cp ./sourcemod/build/package/addons/sourcemod/bin/* ../sourcemod/bin/ -rfv
